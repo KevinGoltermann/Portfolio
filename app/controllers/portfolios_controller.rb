3bc666a7e1621ccb8749ead53072cef2dbd3fc83
KG <kggoltermann@gmail.com>
2021-10-04 13:17:52 -0600

    added edit and update functionalty for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,25 +1,40 @@
+# frozen_string_literal: true
+
 class PortfoliosController < ApplicationController
-	
-	def index
-		@portfolio_items = Portfolio.all
-	end
+  def index
+    @portfolio_items = Portfolio.all
+  end
 
-	def show
-	end
+  def new
+    @portfolio_item = Portfolio.new
+  end
 
-	def new
-		@portfolio_item = Portfolio.new
-	end
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
-	def create
-    	@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your Portfolio Item is now live' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
 
-   		respond_to do |format|
-   			if @portfolio_item.save
-   				format.html { redirect_to portfolios_path, notice: "Your Portfolio Item is now live" }
-   			else
-   				format.html { render :new, status: :unprocessable_entity }
-   			end
-   		end
-   	end
+  def edit 
+  	@portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+  	@portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
