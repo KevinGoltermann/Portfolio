22e72fb9504aa4aa295688b39a4db0e4f3b67246
KG <kggoltermann@gmail.com>
2021-10-07 23:47:20 -0600

    finished creating publish blog feature

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class BlogsController < ApplicationController
-  before_action :set_blog, only: %i[show edit update destroy]
+  before_action :set_blog, only: %i[show edit update destroy toggle_status]
 
   # GET /blogs or /blogs.json
   def index
@@ -52,7 +52,12 @@ class BlogsController < ApplicationController
   end
 
   def toggle_status
-    byebug
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+    redirect_to blogs_url,  notice: 'Post status has been updated'
   end
 
   private
